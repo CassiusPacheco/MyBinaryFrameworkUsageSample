6150ae00b792cb9773d3f01b6cb34c45d70dd99a
Cassius Pacheco <cassius.pacheco@immutable.com>
2022-10-14 09:45:31 +1100

    test1

---
+++ MyBinaryFrameworkWrapper/include/bridge.h
@@ -0,0 +1,12 @@
+#ifndef Bridge_h
+#define Bridge_h
+
+#ifdef __OBJC__
+
+// Add all required headers from the binary framework.
+#import <MyBinaryFramework/MyBinaryFramework-Swift.h>
+#endif
+
+#include <stdio.h>
+
+#endif
--- Package.swift
+++ Package.swift
@@ -23,8 +23,7 @@ let package = Package(
             name: "MyBinaryFrameworkUsageTarget",
             dependencies: [
                 .target(name: "MyBinaryFrameworkWrapper")
-            ],
-            path: "MyBinaryFrameworkUsageTarget"
+            ]
         ),
         .target(
             name: "MyBinaryFrameworkWrapper",
@@ -32,19 +31,24 @@ let package = Package(
                 .target(name: "MyBinaryFramework"),
                 .product(name: "Valet", package: "Valet"),
             ],
-            path: "MyBinaryFrameworkWrapper"
+            path: "MyBinaryFrameworkWrapper",
+            linkerSettings: [
+                .linkedFramework("Security"),
+                .linkedFramework("Foundation")
+            ]
         ),
         // binary built using stack evolution https://github.com/unsignedapps/swift-create-xcframework/blob/main/Sources/CreateXCFramework/XcodeBuilder.swift#L126
-//        .binaryTarget(
-//            name: "MyBinaryFramework",
-//            url: "https://github.com/CassiusPacheco/MyBinaryFramework/releases/download/v0.0.1/MyBinaryFramework-stack.zip",
-//            checksum: "f92a96bde1656d6a10fad511d99f34adcc115bb8cba6f9de640c5cb9f8ab1dea"
-//        ),
-        // same code as above, but binary without stack evolution
         .binaryTarget(
             name: "MyBinaryFramework",
-            url: "https://github.com/CassiusPacheco/MyBinaryFramework/releases/download/v0.0.1/MyBinaryFramework.zip",
-            checksum: "4569d72836bafbe0dc069a5d18af99f9b0bbbf8cf31be552330c79a6d67012fd"
+            url: "https://github.com/CassiusPacheco/MyBinaryFramework/releases/download/v0.0.1/MyBinaryFramework-stack.zip",
+            checksum: "f92a96bde1656d6a10fad511d99f34adcc115bb8cba6f9de640c5cb9f8ab1dea"
         ),
+        // same code as above, but binary without stack evolution
+//        .binaryTarget(
+//            name: "MyBinaryFramework",
+//            url: "https://github.com/CassiusPacheco/MyBinaryFramework/releases/download/v0.0.1/MyBinaryFramework.zip",
+//            checksum: "4569d72836bafbe0dc069a5d18af99f9b0bbbf8cf31be552330c79a6d67012fd"
+//        ),
+//        .binaryTarget(name: "MyBinaryFramework", path: "MyBinaryFramework.xcframework")
     ]
 )
